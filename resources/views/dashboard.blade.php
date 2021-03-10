f26e5dfe52b0d41bd9f6f206566ce65ae8fbbaa0
roslava <roslav@icloud.com>
2021-03-10 14:25:57 +0300

    add widjet quantity of articles on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,28 @@
-
 @extends('layouts.app')
 @section('title', 'Паннель управления')
 
 @section('content')
 
 <div class="content">
-<h3>Паннель управления</h3>
-<h6>Тут можно добавить, изменить или удалить статьи сайта ckk.ru</h6>
-<p>Количество опубликованных статей: {{$quantityOfArticles}}</p>
+
+    <h3>Паннель управления</h3>
+    <h6>Тут можно добавить, изменить или удалить статьи сайта ckk.ru</h6>
+
+
+    <div class="dash-board__wrap">
+        <div class="dash-board__column">
+    <a href="{{ route('articles.index') }}" class="quantity-widget_link">
+            <div class="quantity-widget">
+                <div class="quantity-widget__description">Количество опубликованных статей:</div>
+                <div class="quantity-widget__figure">{{$quantityOfArticles}}</div>
+            </div>
+    </a>
+        </div>
+        <div class="dash-board__column"></div>
+        <div class="dash-board__column"></div>
+
+    </div>
+
 </div>
 
-    <!-- <div class="tw-py-12">
-        <div class="tw-max-w-7xl tw-mx-auto sm:tw-px-6 lg:tw-px-8">
-            <div class="tw-bg-white tw-overflow-hidden tw-shadow-xl sm:tw-rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div> -->
-
-
-    @endsection
-
-
+@endsection
